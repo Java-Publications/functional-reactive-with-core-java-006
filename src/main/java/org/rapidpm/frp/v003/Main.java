910b45b9e71079bf7c8b727e1aca442c66a04152
Sven Ruppert <sven.ruppert@gmail.com>
2017-09-01 10:27:31 +0200

    v003

---
+++ src/main/java/org/rapidpm/frp/v003/Main.java
@@ -0,0 +1,19 @@
+package org.rapidpm.frp.v003;
+
+/**
+ *
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    final String x = "A";
+
+    switch (x) {
+      case "A": break;
+      case "B": break;
+      case "C": break;
+      default : break;
+    }
+  }
+}
